80f1554b6bba71e7724f99d58229f444defed125
sina <sinaolfati5@gmail.com>
2022-07-26 18:07:35 +0430

    making the FirstPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// SPA
+import {Routes, Route} from "react-router-dom"
+// components
+import FirstPage from './components/FirstPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Sina
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<FirstPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/FirstPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./FirstPage.css"
+
+const FirstPage = () => {
+
+  return (
+    <div className='container'>
+      <h1>S</h1>
+      <div className='buttons'>
+        <div className='first'>ساخت حساب کاربری</div>
+        <div>ورود</div>
+      </div>
+    </div>
+  )
+}
+
+export default FirstPage
